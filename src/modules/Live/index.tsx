d821d0d2e1a3b0cfa983de02a4bf6659e5cf8d29
Muhammad Javed <muhammadjaved03616@gmail.com>
2024-06-04 03:00:42 +0500

    added a dummy responses until api CORS issue resolved

--- src/modules/Live/index.tsx
+++ src/modules/Live/index.tsx
@@ -31,6 +31,45 @@ const Live = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
+  // Dummy responses until api CORS issue resolved
+
+  const dummyRes = {
+    message: "Success",
+    status_code: 200,
+    data: {
+      id: 999,
+      channel_name: "19ff913b-951e-48c7-841a-502ae37de09e",
+      channel_settings: {
+        notify: {
+          type: "FOLLOWERS",
+          hide_from: ["f8f2890b-a421-47c2-92f1-f1539597dc95-1711085219869"],
+        },
+        live_type: "duo",
+        visibility: {
+          type: "FOLLOWERS_YOU_FOLLOW_BACK",
+          hide_from: ["28681b71-177c-4fed-b9fa-f306a3735911-1711675498057"],
+        },
+        description: "this is live",
+        allow_comments: false,
+        max_no_of_users: 6,
+      },
+      rtc_token:
+        "007eJxTYMhckiSqZZ/KNYc9ZunN9smx+62VhX2+vGI/fGvuOtmuOyUKDMaGiabmZilJppYmKSZmlhZJBolJhqaJqWZJlsZmJsaGTH2xaQ2BjAzB0XksjAyMDCwMjAwgPhOYZAaTLGBShcHQMi3N0tA4SdfS1DBV18Qi2VzXwsQwUdfUwCgx1dg8JdXAMpWFwdjAyBIA2KUoFA==",
+      rtm_token:
+        "007eJxTYPC45B0hKVFzz3Oz95LdojkXewN/eyiI5Pyb37NbdKPqHT4FBmPDRFNzs5QkU0uTFBMzS4skg8QkQ9PEVLMkS2MzE2NDpr7YtIZARoYPqqcYGBmYGBgZGBlAfBYGYwMjSwCkix0J",
+      user_details: {
+        host: {
+          user_id: "5d5a54fa-1d0f-4c79-9972-55286d5447c6-1714807976028",
+          agora_user_id: 3029,
+          username: "waqas",
+        },
+        co_host: [],
+        audience: [],
+        audience_total: 0,
+      },
+    },
+  };
+
   return (
     <div>
       <h1>Channel Details</h1>
